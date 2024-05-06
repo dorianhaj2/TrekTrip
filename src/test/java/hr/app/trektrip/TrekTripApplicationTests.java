af93d9390ea611ce81c9833acdf6c0cbedea748a
dorianhaj <dorianhajnic@gmail.com>
2024-05-06 22:53:30 +0200

    Initial commit

---
+++ src/test/java/hr/app/trektrip/TrekTripApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.app.trektrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
