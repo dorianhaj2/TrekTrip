eedb03dad906e412bcac1cb176b6362bb1f323da
dorianhaj2 <78280436+dorianhaj2@users.noreply.github.com>
2024-05-09 17:35:41 +0200

    Initial commit

---
+++ src/main/java/com/trektrip/TrektripApplication.java
@@ -0,0 +1,13 @@
+package com.trektrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrektripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrektripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trektrip/TrektripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trektrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrektripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
