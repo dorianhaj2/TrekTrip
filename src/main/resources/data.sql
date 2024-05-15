8aa08fe298a44eedc4440c0c1d5c9d6ae07a0897
Luka58 <luka.culina56@gmail.com>
2024-05-16 01:18:06 +0200

    Tried out JetClient drafts

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into countries(id, name, continent) values (1,'Croatia','Europe');
+
+insert into countries(id, name, continent) values (2, 'Germany', 'Europe');
+
+insert into countries(id, name, continent) values (3, 'Japan', 'Asia');
+
+insert into countries(id, name, continent) values (4, 'China', 'North America')
