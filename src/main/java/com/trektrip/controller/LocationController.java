76caad50f7a777cc0527998177f45924f27246b6
KarloKrivohlavek <karlo.krivohlavek@hotmail.com>
2024-05-16 14:59:54 +0200

    Location and Pin logic added

---
+++ src/main/java/com/trektrip/controller/LocationController.java
@@ -0,0 +1,62 @@
+package com.trektrip.controller;
+
+import com.trektrip.trektrip.model.Location;
+import com.trektrip.trektrip.service.LocationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/location")
+@AllArgsConstructor
+
+public class LocationController {
+
+    private LocationService locationService;
+
+    @GetMapping("/all")
+    public List<Location> getAllLocations() {
+       return locationService.getLocations();
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Location> getLocationById(@PathVariable Long id) {
+        Optional<Location> locationOptional =  locationService.getLocation(id);
+        if (locationOptional.isPresent()){
+            return ResponseEntity.ok(locationOptional.get());
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Location> createLocation(@RequestBody Location location) {
+        return new ResponseEntity<>(locationService.createLocation(location), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Location> updateLocation(@RequestBody Location location, @PathVariable Long id) {
+        try {
+            Location updatedlocation = (Location) locationService.updateLocation(location, id);
+            return new ResponseEntity<>(updatedlocation, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocation(@PathVariable Long id) {
+        locationService.deleteLocation(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
+
+
---
+++ src/main/java/com/trektrip/service/LocationServiceImpl.java
@@ -0,0 +1,52 @@
+package com.trektrip.service;
+
+import com.trektrip.trektrip.model.Location;
+import com.trektrip.trektrip.repository.LocationRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+
+public class LocationServiceImpl implements LocationService {
+
+    private LocationRepository locationRepository;
+
+    @Override
+    public List<Location> getLocations() {
+        return locationRepository.findAll();
+    }
+
+    @Override
+    public Optional<Location> getLocation(Long id) {
+        return locationRepository.findById(id);
+    }
+
+    @Override
+    public Location createLocation(Location location) {
+        return locationRepository.save(location);
+    }
+
+    @Override
+    public Location updateLocation(Location location, Long id) {
+        Optional<Location> locationOptional = locationRepository.findById(id);
+        if (locationOptional.isPresent()) {
+            Location locationToUpdate = locationOptional.get();
+            locationToUpdate.setCity(location.getCity());
+            return locationRepository.save(locationToUpdate);
+        } else {
+            throw new EntityNotFoundException("Location with the ID '"+ id + "' not found");
+
+        }
+
+    }
+
+    @Override
+    public void deleteLocation(Long id) {
+        locationRepository.deleteById(id);
+    }
+}
