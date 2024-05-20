4a0ae4fe72e84225e493a0eadc02dc96380c9638
dorianhaj2 <78280436+dorianhaj2@users.noreply.github.com>
2024-05-20 20:14:27 +0200

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-insert into countries(id, name, continent) values (1,'Croatia','Europe');
-
-insert into countries(id, name, continent) values (2, 'Germany', 'Europe');
-
-insert into countries(id, name, continent) values (3, 'Japan', 'Asia');
-
-insert into countries(id, name, continent) values (4, 'China', 'North America')
