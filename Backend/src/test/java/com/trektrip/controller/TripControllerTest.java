d16c9731068242ff293ae2273993cefb580f8c34
Luka Čulina <106603546+LukaCulina@users.noreply.github.com>
2024-06-13 14:32:07 +0200

    Create TripControllerTest.java

---
+++ Backend/src/test/java/com/trektrip/controller/TripControllerTest.java
@@ -0,0 +1,137 @@
+package com.trektrip.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trektrip.model.Trip;
+import com.trektrip.service.TripService;
+import com.trektrip.service.JwtService;
+import com.trektrip.service.UserDetailsServiceImpl;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+@WebMvcTest(controllers = TripController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+class TripControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TripService tripService;
+
+    @MockBean
+    private JwtService jwtService;
+    @MockBean
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Trip trip1;
+    private Trip trip2;
+
+    @BeforeEach
+    public void init() {
+        trip1 = new Trip(1L, "Trip 1", "Description 1", 5, true);
+        trip2 = new Trip(2L, "Trip 2", "Description 2", 7, false);
+    }
+
+    @Test
+    @WithMockUser
+    public void testCreateTrip() throws Exception{
+        given(tripService.createTrip(ArgumentMatchers.any())).willAnswer((invocationOnMock -> invocationOnMock.getArgument(0)));
+
+        ResultActions response = mockMvc.perform(post("/trip")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(trip1)));
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(trip1.getTitle())));
+    }
+
+    @Test
+    @WithMockUser
+    public void testGetAllTrips() throws Exception {
+        List<Trip> allTrips = List.of(trip1, trip2);
+        when(tripService.getAllTrips()).thenReturn(allTrips);
+
+        ResultActions response = mockMvc.perform(get("/trip/all")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(allTrips.size())));
+    }
+
+    @Test
+    @WithMockUser
+    public void testGetTripByIdIfExists() throws Exception {
+        Long id = 1L;
+        when(tripService.getTripById(id)).thenReturn(Optional.of(trip1));
+
+        ResultActions response = mockMvc.perform(get("/trip/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(trip1)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(trip1.getTitle())));
+    }
+
+    @Test
+    @WithMockUser
+    public void testGetTripByIdDoesntExist() throws Exception {
+        Long id = 3L;
+        when(tripService.getTripById(id)).thenReturn(Optional.empty());
+
+        ResultActions response = mockMvc.perform(get("/trip/3")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    @WithMockUser
+    public void testUpdateTrip() throws Exception {
+        Long id = 1L;
+        when(tripService.updateTrip(trip2, id)).thenReturn(trip2);
+
+        ResultActions response = mockMvc.perform(put("/trip/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(trip2)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", CoreMatchers.is(trip2.getTitle())));
+    }
+
+    @Test
+    @WithMockUser
+    public void testDeleteTrip() throws Exception {
+        Long id = 1L;
+        doNothing().when(tripService).deleteTrip(id);
+
+        ResultActions response = mockMvc.perform(delete("/trip/1")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+}
